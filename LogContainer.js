9c79bb2db372e5ea24a84b74f4048bbee4b27170
Ezequiel Girola <ezequielgirola@gmail.com>
2021-05-11 14:35:30 -0300

    More log to time

--- LogContainer.js
+++ LogContainer.js
@@ -121,6 +121,7 @@ module.exports = class {
 
             for (const wallet of wallets) {
                 let rawLogs = await this.pool.query(`SELECT unpaid_balance, average_hashrate, created_date FROM log_table WHERE log_table.wallet = '${wallet.getWallet()}'`)
+                console.log('RawAllLogs: ', rawLogs);
                 let logs = this.convertRawToLogs(rawLogs.rows);
                 wallet.logs = logs;
             }
