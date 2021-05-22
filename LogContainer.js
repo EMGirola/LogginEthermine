7757ef120ee8f06614528e075b59a095dd6269a0
Ezequiel Girola <ezequielgirola@gmail.com>
2021-05-22 13:49:46 -0300

    Need to construct a standard value of time

--- LogContainer.js
+++ LogContainer.js
@@ -45,10 +45,13 @@ module.exports = class {
                     && rawData.data.time
                     ) {
 
+                    let dateToInsert = new Date().toLocaleString("es-ES", { timeZone: 'America/Argentina/Buenos_Aires'});
+                    dateToInsert = dateToInsert.replace(" ", "T")
+                    dateToInsert = dateToInsert + '.000Z';
                     let unpaid = rawData.data.unpaid;
                     let averageHashrate = rawData.data.averageHashrate;
-                    let values = [wall.getWallet(), unpaid, averageHashrate, new Date().toLocaleString("es-ES", { timeZone: 'America/Argentina/Buenos_Aires'})];
-                    this.pool.query('INSERT INTO log_table(wallet, unpaid_balance, average_hashrate, created_date) values ($1, $2, $3, $4)', values);
+                    let values = [wall.getWallet(), unpaid, averageHashrate, dateToInsert];
+                    await this.pool.query('INSERT INTO log_table(wallet, unpaid_balance, average_hashrate, created_date) values ($1, $2, $3, $4)', values);
                     cont++;
                 }
             }
